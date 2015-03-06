df39efe0ed9271bd1de502139b7ac812ca4791e4
isozumi <t_isozumi@hotmail.com>
2015-03-06 12:05:46 +0900

    spec3: レシピ情報をファイルから読み込むように修正

--- src/SeminarClass1.java
+++ src/SeminarClass1.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  */
@@ -14,8 +19,30 @@ public class SeminarClass1 {
 	 */
 	public static void main(String[] args) {
 
+		/////////////////////////////////////
+		//変数宣言
+		String output=null;					//メッセージ出力用変数
+		String dataFilePath=null;
+
+
+		/////////////////////////////////////
+		//ファイル読み込み
+		FileReader fileReader = null;
+		try {
+			fileReader = new FileReader("data/data.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		BufferedReader bufferedReader=new BufferedReader(fileReader);
+
+		try {
+			output = bufferedReader.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		//実行結果出力
-		System.out.println("オムライス");
+		System.out.println(output);
 
 	}
 
