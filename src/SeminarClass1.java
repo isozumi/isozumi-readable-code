4389cd13974072f3106ba8011af1fc0e5e111f51
isozumi <t_isozumi@hotmail.com>
2015-03-06 11:09:20 +0900

    siyou1

---
+++ src/SeminarClass1.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+
+/**
+ * @author isozumi
+ *
+ */
+public class SeminarClass1 {
+
+	/**
+	 * @param args
+	 * オムライスのレシピを出力する
+	 */
+	public static void main(String[] args) {
+		System.out.println("オムライス");
+
+	}
+
+}
